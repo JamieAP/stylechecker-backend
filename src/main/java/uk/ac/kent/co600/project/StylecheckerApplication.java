cdf3b7d3fa2d6a90c6e1c5ffcbc1770de6a2a93d
Jamie Perkins <mail@jperkins.co.uk>
2015-11-12 21:15:39 +0000

    Avoid deprecated config loading method

--- src/main/java/uk/ac/kent/co600/project/StylecheckerApplication.java
+++ src/main/java/uk/ac/kent/co600/project/StylecheckerApplication.java
@@ -3,22 +3,18 @@ package uk.ac.kent.co600.project;
 import com.google.common.base.Throwables;
 import com.puppycrawl.tools.checkstyle.Checker;
 import com.puppycrawl.tools.checkstyle.ConfigurationLoader;
-import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
-import com.puppycrawl.tools.checkstyle.PropertiesExpander;
-import com.puppycrawl.tools.checkstyle.api.*;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
+import org.apache.commons.io.IOUtils;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.xml.sax.InputSource;
 import uk.ac.kent.co600.project.api.http.CheckerResource;
 import uk.ac.kent.co600.project.jar.JarExtractor;
 
-import java.io.File;
-import java.io.InputStream;
-import java.util.List;
-import java.util.SortedSet;
-
 public class StylecheckerApplication extends Application<StylecheckerConfiguration> {
+
+    private static final String CHECKSTYLE_CONFIG_FILE_NAME = "checkstyle-configuration.xml";
 
     public static void main(String[] args) throws Exception {
         new StylecheckerApplication().run(args);
@@ -33,23 +29,21 @@ public class StylecheckerApplication extends Application<StylecheckerConfigurati
 
     private Checker initializeCheckstyle() {
         try {
+            InputSource checkstyleConfigXml = new InputSource(
+                    ClassLoader.getSystemResourceAsStream(CHECKSTYLE_CONFIG_FILE_NAME)
+            );
             Checker checker = new Checker();
             checker.setModuleClassLoader(ClassLoader.getSystemClassLoader());
-            checker.configure(ConfigurationLoader.loadConfiguration(loadCheckstyleConfiguration(), null, false));
+            checker.configure(ConfigurationLoader.loadConfiguration(checkstyleConfigXml, null, true));
             return checker;
         } catch (Exception e) {
             throw Throwables.propagate(e);
         }
     }
 
-    private InputStream loadCheckstyleConfiguration() {
-        return ClassLoader.getSystemResourceAsStream("checkstyle-configuration.xml");
-    }
-
     private AbstractBinder instanceBindings() {
         return new AbstractBinder() {
-            @Override
-            protected void configure() {
+            @Override protected void configure() {
                 bind(new JarExtractor()).to(JarExtractor.class);
                 bind(initializeCheckstyle()).to(Checker.class);
             }
