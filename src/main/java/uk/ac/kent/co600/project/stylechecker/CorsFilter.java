c83bdb94329a7ff779cbd13bbf2c29d42962bfce
Jamie Perkins <mail@jperkins.co.uk>
2015-12-14 20:58:25 +0000

    Use Http constants

--- src/main/java/uk/ac/kent/co600/project/stylechecker/CorsFilter.java
+++ src/main/java/uk/ac/kent/co600/project/stylechecker/CorsFilter.java
@@ -1,7 +1,10 @@
 package uk.ac.kent.co600.project.stylechecker;
 
+import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
+import com.google.common.net.HttpHeaders;
 
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -11,24 +14,35 @@ import java.io.IOException;
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
-    private static final String OPTIONS = "OPTIONS";
+    private static final Joiner JOINER = Joiner.on(", ");
 
     @Override
     public void filter(ContainerRequestContext reqCtx, ContainerResponseContext resCtx) throws IOException {
-        if (reqCtx.getMethod().equals(OPTIONS)) {
+        if (reqCtx.getMethod().equals(HttpMethod.OPTIONS)) {
             resCtx.setEntity("");
             resCtx.setStatus(200);
 
-            resCtx.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST");
-            resCtx.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-            resCtx.getHeaders().putSingle("Access-Control-Max-Age", "1800");
+            resCtx.getHeaders().putSingle(
+                    HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS,
+                    JOINER.join(HttpMethod.GET, HttpMethod.POST)
+            );
+            resCtx.getHeaders().putSingle(
+                    HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
+                    JOINER.join(
+                            HttpHeaders.ORIGIN,
+                            HttpHeaders.CONTENT_TYPE,
+                            HttpHeaders.ACCEPT,
+                            HttpHeaders.AUTHORIZATION
+                    )
+            );
+            resCtx.getHeaders().putSingle(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "1800");
         }
 
-        String domain = reqCtx.getHeaderString("Origin");
+        String domain = reqCtx.getHeaderString(HttpHeaders.ORIGIN);
 
         if (!Strings.isNullOrEmpty(domain)) {
-            resCtx.getHeaders().putSingle("Access-Control-Allow-Origin", domain);
-            resCtx.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+            resCtx.getHeaders().putSingle(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, domain);
+            resCtx.getHeaders().putSingle(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, true);
         }
     }
 }
