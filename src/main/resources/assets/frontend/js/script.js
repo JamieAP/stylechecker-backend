6b6d41aaec2007e49086a98151ccf6e9f608baae
Jamie Perkins <mail@jperkins.co.uk>
2016-03-18 12:08:35 +0000

    Updated endpoints in frontend assets

--- src/main/resources/assets/frontend/js/script.js
+++ src/main/resources/assets/frontend/js/script.js
@@ -59,7 +59,7 @@ function submitForm(formData) {
             }, false);
             return xhr;
         },
-        url: 'http://stylechecker.jkeeys.co.uk:8888/stylechecker/api/check',
+        url: 'http://localhost:8888/stylechecker/api/check',
         type: 'POST',
         data: formData,
         async: true,
