d75e2e5862e0b94b773c2e26b92982d5e678bf06
Jamie Perkins <mail@jperkins.co.uk>
2016-03-27 17:41:01 +0100

    Remove auto loading of config from classpath

--- src/main/java/uk/ac/kent/co600/project/stylechecker/StylecheckerApplication.java
+++ src/main/java/uk/ac/kent/co600/project/stylechecker/StylecheckerApplication.java
@@ -1,8 +1,6 @@
 package uk.ac.kent.co600.project.stylechecker;
 
-import com.google.common.base.Strings;
 import com.google.common.base.Throwables;
-import com.google.common.collect.ImmutableList;
 import com.puppycrawl.tools.checkstyle.ConfigurationLoader;
 import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 import com.puppycrawl.tools.checkstyle.api.Configuration;
@@ -22,37 +20,12 @@ import uk.ac.kent.co600.project.stylechecker.checkstyle.audit.AuditScorer;
 import uk.ac.kent.co600.project.stylechecker.jar.SourcesJarExtractor;
 import uk.ac.kent.co600.project.stylechecker.utils.FauxHealthCheck;
 
-import java.util.Arrays;
-
 public class StylecheckerApplication extends Application<StylecheckerConfiguration> {
 
     private static final String CHECKSTYLE_CONFIG_FILE = "checkstyle-configuration.xml";
-    private static final String STYLECHECKER_CONFIG_FILE = "stylechecker-config.yml";
-    private static final String SERVER = "server";
 
     public static void main(String[] cliArgs) throws Exception {
-        if (!Strings.isNullOrEmpty(cliArgs[0]) && cliArgs[0].equals(SERVER)) {
-            cliArgs = addConfigFromClasspath(cliArgs);
-        }
         new StylecheckerApplication().run(cliArgs);
-    }
-
-    /*
-        This adds the path for the default server configuration to the CLI arguments,
-        allowing Dropwizard to load it from the JAR without it being specified by the user
-
-        We do this as there is no way to override the default server configuration without
-        specifying a configuration file on the CLI
-
-        Configuration can still be overridden using system properties,
-        e.g. -Ddw.server.connector.port=9090
-     */
-    private static String[] addConfigFromClasspath(String[] cliArgs) {
-        return ImmutableList.<String>builder()
-                    .addAll(Arrays.asList(cliArgs))
-                    .add(STYLECHECKER_CONFIG_FILE)
-                    .build()
-                    .toArray(new String[cliArgs.length + 1]);
     }
 
     @Override
@@ -67,7 +40,6 @@ public class StylecheckerApplication extends Application<StylecheckerConfigurati
 
     @Override
     public void initialize(Bootstrap<StylecheckerConfiguration> bootstrap) {
-        bootstrap.setConfigurationSourceProvider(new ResourceConfigurationSourceProvider());
         bootstrap.addBundle(new AssetsBundle("/assets/frontend/", "/", "index.html"));
         bootstrap.addCommand(
                 new CheckerCommand(
